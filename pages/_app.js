29cdb5898e628a77134e562b10b62164753fe5f6
ZeroToH3ro <mtblaser2002@gmail.com>
2024-03-23 10:58:53 +0700

    build smart contract

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+
+
+//INTERAL IMPORT 
+import { TrackingProvider } from "../Context/Tracking";
+import {NavBar, Footer} from "../Components";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <TrackingProvider>
+        <NavBar/>
+        <Component {...pageProps} />
+      </TrackingProvider>
+      <Footer/>
+    </>
+  )
+}
